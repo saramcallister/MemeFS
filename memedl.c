ae5b2a3dda9b325ebce2e98ecb3d5e1038c6e9fe
meow <mjpalanker@gmail.com>
2018-03-27 13:59:52 -0700

    CAN DOWNLOAD MEMES! new url2file lets us download memes wrote initial tests to tie together url parsing and downloading urls

--- memedl.c
+++ memedl.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include "getinmemory.c"
 #include "urlextract.c"
+#include "url2file.c"
 
 #define RSSFEED "https://www.reddit.com/r/dankmemes/.rss?sort=new"
 
+#define FOLDRNAME "memes/"
 
 
 /* pass me an n and a buffer of size (buffer[n][MAX_MATCH]) */
@@ -19,16 +21,27 @@ int nmemeurls(int n, char* buffer)
   return r;
 }
 
+static char *filenameget(const char* url)
+{
+  return strrchr(url, '/') + 1;
+}
 
 int main(int argc, char const *argv[])
 {
+  const char folder[] = FOLDRNAME;
+  char dest[MAX_MATCH];
   char buffer[20][MAX_MATCH];
   int i;
   int r;
+  strcpy((char*)&dest,(char*)&folder);
   r = nmemeurls(20,(char*)&buffer);
   for (i = 0; i < r; i++)
   {
     printf("got: %s\n", buffer[i]);
+    strcat((char*)&dest, filenameget(buffer[i]));
+    printf("Saving to: %s\n", (char*)&dest);
+    url2file(buffer[i],(char*)&dest);
+    strcpy((char*)&dest,(char*)&folder);
   }
   return 0;
 }
